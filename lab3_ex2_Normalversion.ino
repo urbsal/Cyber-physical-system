00e09e6d7fede2ce29557b68df2a84981e3e7038
urbsal <bsal.stha.bs@gmail.com>
2025-11-13 16:39:34 +0200

    Add files via upload

---
+++ lab3_ex2_Normalversion.ino
@@ -0,0 +1,21 @@
+int led = 13;
+
+
+void setup()
+{
+    Serial.begin(9600);
+   pinMode(13,OUTPUT);
+
+
+}
+
+void loop()
+{
+   digitalWrite(13, LOW);
+   delay(1000);
+   digitalWrite(13,HIGH);
+   delay(100);
+}
+
+   
+
